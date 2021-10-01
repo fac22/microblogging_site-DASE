2d640f68abf7386294e39c4245e145d68610d9ed
cerealenjoyer <sasha.antilyons@gmail.com>
2021-10-01 11:53:57 +0100

    Modularisation and bugfixing

--- routes/addPost.js
+++ routes/addPost.js
@@ -1,44 +1,26 @@
 const posts = require('../posts');
+const layout = require('../body');
 
 function get(request, response) {
-  const html = `
-  <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="microblogging project">
-    <link rel="stylesheet" href="styles.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&display=swap"
-      rel="stylesheet">
-      <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
+  const html = /* html */ `
+    <header>
+      <h1>Add your post</h1>
+    </header>
+    <main>
+      <div class="center box">
+        <form method="POST">
+          <label for="name"><h2>Your name</h2></label>
+          <input id="name" name="name" />
+          <label for="post"><h2>Your post</h2> </label>
+          <input id="post" name="post" required/>
+          <button>Add</button>
+        </form>
+      </div>
+    </main>
+  `;
 
-    <title>Shortlr</title>
-        </head>
-        <body>
-        <header>
-          <h1>Add your post</h1>
-         </header>
-         <main>
-         <div class="center box"> 
-          <form method="POST">
-            <label for="name"><h2>Your name</h2></label>
-            <input id="name" name="name">
-            <label for="post"><h2>Your post</h2> </label>
-            <input id="post" name="post">
-            <button>Add</button>
-          </form>
-          </div>
-          </main>
-        </body>
-      </html>
-      `;
-  response.send(html);
+  const page = layout.htmlpopulate('Shortlr', html);
+  response.send(page);
 }
 
 let idArr = [];
@@ -52,7 +34,7 @@ function post(request, response) {
     idArr = Object.keys(posts);
     // eslint-disable-next-line radix
     const idArrInt = idArr.map((el) => parseInt(el));
-    let bigId = idArrInt.reduce((acc, cur) => (acc > cur ? acc : cur), 0);
+    const bigId = idArrInt.reduce((acc, cur) => (acc > cur ? acc : cur), 0);
 
     id = bigId + 1;
     posts[id] = newPost;
@@ -62,24 +44,7 @@ function post(request, response) {
 
 function error(request, response) {
   const html = `
-  <!DOCTYPE html>
-  <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="microblogging project">
-        <link rel="stylesheet" href="/styles.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&display=swap"
-          rel="stylesheet">
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
-      <title>Error</title>
-    </head>
-    <body>
+
     <header>
       <h1>Submission error</h1>
     </header>
@@ -87,10 +52,9 @@ function error(request, response) {
       <h2> That is way too long, no-one wants to read that </h2>
       <h3> <a href="/add-post">I'll be better, take me back</a> </h3>
       </div>
-    </body>
-  </html>
   `;
-  response.send(html);
+  const page = layout.htmlpopulate('Error', html);
+  response.send(page);
 }
 
 module.exports = { get, post, error };
